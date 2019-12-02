7adfc031d7a504a689fbb38f15d7a0c8e120824f
manuelwecker <manuelwecker@web.de>
2019-12-02 14:53:24 +0100

    Add passwords JSON

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
-// Import mit require
 const { get, set, unset } = require("./utils/cmds");
 
-console.log("hallo password manager");
 const userArgs = process.argv.slice(2);
 const [cmd, key, value] = userArgs;
 
 switch (cmd) {
-  case "get": {
-    const result = get(key);
-    console.log(result);
+  case "get":
+    {
+      const result = get(key);
+      console.log(result);
+    }
     break;
-  }
-  case "set": {
+
+  case "set":
     set(key, value);
     break;
-  }
+
   case "unset":
     unset(key);
     break;
 
   default:
-    console.error("unknown commando");
-    break;
+    console.error("Unknown command");
 }
-
-console.log("done");
